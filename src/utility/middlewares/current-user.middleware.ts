002708149ba0b50b3904e4d48623127def6c9126
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-11 21:51:25 +0500

    feat(middleware): create cuurentUser middleware

---
+++ src/utility/middlewares/current-user.middleware.ts
@@ -0,0 +1,44 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { isArray } from 'class-validator';
+import { verify } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { UserEntity } from 'src/users/entities/user.entity';
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserEntity
+        }
+    }
+}
+
+
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+    constructor(
+        private readonly userService: UsersService
+    ) { }
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const authHeader = req.headers.authorization || req.headers.Authorization;
+        if (!authHeader || isArray(authHeader) || !authHeader.startsWith('Bearer ')) {
+            const currentUser = null;
+            next();
+        } else {
+            const token = authHeader.split(' ')[1]
+            const { id } = <JwtPayload>verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+            const currentUser = await this.userService.findOne(+id);
+            req.currentUser = currentUser;
+
+            next();
+        }
+
+
+    }
+}
+
+interface JwtPayload {
+    id: string;
+}
