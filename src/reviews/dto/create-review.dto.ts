214da188218baa0fd39b16aaa4059bd73f449c90
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-14 11:16:49 +0500

    feat(review): create review dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,16 @@
-export class CreateReviewDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateReviewDto {
+
+    @IsNotEmpty({ message: "Ratings is must not be empty" })
+    @IsNumber({}, { message: "Rating should be number" })
+    ratings: number
+
+    @IsNotEmpty({ message: "Comment is must not be empty" })
+    @IsString({ message: "Comment is should be string" })
+    comment: string
+
+    @IsNotEmpty({ message: "Product id must not be empty!!!" })
+    @IsNumber({}, { message: "Product id should be number" })
+    productId: number
+}
