5e5f3d917f20e2db9fb48f876232289f82f291c6
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-23 13:15:22 +0500

    feat(orders): order entity fields

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
 import { CategoryEntity } from "src/categories/entities/category.entity";
+import { OrdersProductsEntity } from "src/orders/entities/orders-products.entity";
 import { ReviewEntity } from "src/reviews/entities/review.entity";
 import { UserEntity } from "src/users/entities/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, Timestamp } from "typeorm";
@@ -38,4 +39,7 @@ export class ProductEntity {
 
     @OneToMany(() => ReviewEntity, (rev) => rev.product)
     reviews: ReviewEntity[]
+
+    @OneToMany(() => OrdersProductsEntity, op => op.product)
+    products: OrdersProductsEntity[]
 }
