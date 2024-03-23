15fb6f12dedf12199b301b0b8f5255533b22da9a
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-23 13:14:32 +0500

    feat(orders): order entity fields

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,35 @@
-export class Order {}
+import { UserEntity } from "src/users/entities/user.entity";
+import { OrderStatus } from "src/utility/enums/order-status.enum";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn, Timestamp } from "typeorm";
+import { ShippingEntity } from "./shipping.entity";
+import { OrdersProductsEntity } from "./orders-products.entity";
+
+@Entity({ name: 'orders' })
+export class OrderEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @CreateDateColumn()
+    orderAt: Timestamp
+
+    @Column({ type: 'enum', enum: OrderStatus, })
+    status: string
+
+    @Column({ nullable: true })
+    shippedAt: Date
+
+    @Column({ nullable: true })
+    deliveredAt: Date
+
+    @ManyToOne(() => UserEntity, user => user.ordersUpdateBy)
+    updatedBy: UserEntity
+
+    @OneToOne(() => ShippingEntity, shipping => shipping.order, { cascade: true })
+    @JoinColumn()
+    shippingAddress: ShippingEntity
+
+    @OneToMany(() => OrdersProductsEntity, op => op.order, { cascade: true })
+    products: OrdersProductsEntity[]
+
+}
