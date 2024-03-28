d8e2038a105569dff69227f029f89e5e94e7d303
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-28 16:36:37 +0500

    feat(orders): create order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,15 @@
-export class CreateOrderDto {}
+import { Type } from "class-transformer";
+import { CreateShippingDto } from "./create-shipping.dto";
+import { OrderedProductsDto } from "./ordered-products.dto";
+import { ValidateNested } from "class-validator";
+
+export class CreateOrderDto {
+
+    @Type(()=>CreateShippingDto)
+    @ValidateNested()
+    addressShipping: CreateShippingDto
+
+    @Type(()=>OrderedProductsDto)
+    @ValidateNested()
+    orderedProducts: OrderedProductsDto[]
+}
